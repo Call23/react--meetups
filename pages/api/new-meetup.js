710a3358e5d5a52ffc85602db875fb8e7866c6d4
Call23 <silasmugendi2019@gmail.com>
2022-05-16 12:45:26 +0300

    fall back

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -12,7 +12,6 @@ async function handler(req, res) {
     const meetupsCollection = db.collection("meetups");
 
     const result = await meetupsCollection.insertOne({ data });
-    console.log(result);
 
     client.close();
 
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -19,8 +19,6 @@ function NewMeetupPage() {
 
     const data = await response.json();
 
-    console.log(data);
-
     router.push("/");
   }
 
